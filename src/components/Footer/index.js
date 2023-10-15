ab90cc1428bae6cd0ab0cc0b7ec8a713e7c58dc7
Guimeb <rm551981@fiap.com.br>
2023-10-15 05:10:53 -0300

    Ajustando coisas do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,10 +15,7 @@ const Base = styled.div`
     box-shadow: 0em 0.3125em 0.625em 0em #aaa;
     z-index: 2;
 `
-const ContainerFooter = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+
 const Contato = styled.div`
     .he { 
     text-align: start;
@@ -40,51 +37,74 @@ const BotaoEmail = styled.img`
     margin-right: 0.5em;
 `
 
+const Links = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const ContainerOp = styled.div`
+    display: flex;
+`
+
+const Opcoes = styled.ul`
+    list-style: none;
+    padding-right: 1em;
+    padding-top: 0.5em;
+    li{
+        cursor: pointer;
+        color: #030303;
+    }
+    li:hover{
+        color: #e69d16;
+    }
+`
+
+const SobreNos = styled.div`
+    text-align: center;
+    width: 25em;
+`
 const Direitos = styled.div`
-    font-size: 10px;
+    text-align: center;
     p {
         align-text: center
     }
 `
-
-const Links = styled.div``
-
-const SobreNos = styled.div`
-    text-align: center;
-    margin: 10em;
-`
-
-const exploreOpcoes = ['Home', 'Sobre', 'Contato', 'Blog', 'FAQ']
 
 function Footer() {
     return (
         <Base>
             <Contato>
                 <h3>Fale Conosco</h3>
-                <BotaoWPP href=""
+                <BotaoWPP href="/"
                     src={whatsapp}
                     alt='contato wpp'>
                 </BotaoWPP>
-                <BotaoEmail href=""
+                <BotaoEmail href="/"
                     src={email}
                     alt='contato email'>
                 </BotaoEmail>
             </Contato>
             <Links>
-                <a>Explore</a>
-                <ul className='opcoes'>
-                    {exploreOpcoes.map((texto) => (
-                        <li className='opcao'><p>{texto}</p></li>
-                    ))}
-                </ul>
+                <h3>Explore</h3>
+                <ContainerOp>
+                    <Opcoes>
+                        <li>Home</li>
+                        <li>Sobre</li>
+                        <li>Contato</li>
+                    </Opcoes>
+                    <Opcoes>
+                        <li>Blog</li>
+                        <li>FAQ</li>
+                    </Opcoes>
+                </ContainerOp>
             </Links>
             <SobreNos>
                 O grupo galo busca estufar trazer conforto e acessibilidade a sociedade. A Vinheria Galo faz isso através do consumo de um bom vinho.
             </SobreNos>
             <Direitos>
-                <a>Vinheria Galo é uma marca oficial da Galo Enterprise.</a>
-                <a><p>Todos os direitos reservados.</p></a>
-                <a><p> 2022-2025</p></a>
+                <p>Vinheria Galo é uma marca oficial da Galo Enterprise.</p>
+                <p>Todos os direitos reservados.</p>
+                <p> 2022-2025</p>
             </Direitos>
         </Base>
     )
